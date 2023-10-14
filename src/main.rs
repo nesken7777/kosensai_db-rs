662e3037ded94dc3564e79d87a738c2448a04b3b
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2023-10-14 15:11:25 +0900

    ポート番号を引数で指定する
    様々な失敗をした場合のデフォルトは80番

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use axum::{routing::get, Router};
 use mysql_async::{OptsBuilder, Pool};
-use std::{error::Error, sync::OnceLock};
+use std::{
+    env::args,
+    error::Error,
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+    sync::OnceLock,
+};
 mod check_and_create;
 mod individual_rank;
 mod name_insert;
@@ -17,6 +22,9 @@ static CELL: OnceLock<Pool> = OnceLock::new();
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn Error>> {
+    let port_num = args()
+        .nth(1)
+        .map_or(80, |num_str| num_str.parse::<u16>().unwrap_or(80));
     let opts = OptsBuilder::default()
         .user(Some("uk"))
         .pass(Some("1234"))
@@ -33,9 +41,12 @@ async fn main() -> std::result::Result<(), Box<dyn Error>> {
         .route("/read.php", get(read))
         .route("/score.php", get(score))
         .route("/individual_rank.php", get(individual_rank));
-    axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    axum::Server::bind(&SocketAddr::V4(SocketAddrV4::new(
+        Ipv4Addr::new(0, 0, 0, 0),
+        port_num,
+    )))
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
     Ok(())
 }
